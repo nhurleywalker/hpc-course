c3ae43e9f1beb7e31bfa9ecf3bb4ab4e20bd8ac6
Dr Natasha Hurley-Walker <nhw@icrar.org>
2023-03-28 17:11:25 +0800

    Example xargs use

---
+++ example_xargs.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Make an input.txt made of a bunch of numbers
+
+if [[ ! -e input.txt ]]
+then
+    for i in {102809..109092}
+    do
+        echo $i >> input.txt
+    done
+fi
+
+# Make a script that squares each number
+
+# From pipe
+#echo 'while read x ; do echo $((x * x)) ; done' > square2.sh
+
+# As an argument
+echo 'n=$1; echo $((n * n))' > square.sh
+
+# See how long that takes without parallelisation
+
+time xargs -P1 -n1 -a input.txt ./square.sh > output.txt
+
+# And with:
+
+time xargs -P4 -n1 -a input.txt ./square.sh > output.txt
+
+
