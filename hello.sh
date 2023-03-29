0e63ef9839fd68797c2b282006cd8f0f6af94bdc
Natasha Hurley-Walker <nhw@icrar.org>
2023-03-29 13:26:14 +0800

    SLURM header for Setonix

--- hello.sh
+++ hello.sh
@@ -3,11 +3,11 @@
 #SBATCH --account=courses0100
 #SBATCH -t 00:10:00
 #SBATCH --nodes=2
-#SBATCH --ntasks=37
-#SBATCH -p workq
+#SBATCH --ntasks=12
+#SBATCH -p work
 
 module load python
-module load mpi4py
+module load py-mpi4py
 
 set -ex
-srun -n 37 python hello.py
+srun -n 12 python hello.py
