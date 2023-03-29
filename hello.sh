b9ca0d465ab42438b3135c3f79157a9d4624ec88
Dr Natasha Hurley-Walker <nhw@icrar.org>
2023-03-29 13:22:28 +0800

    Slight modifications in SLURM header for this year

--- hello.sh
+++ hello.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-
 #SBATCH --export=NONE
 #SBATCH --account=courses0100
 #SBATCH -t 00:10:00
@@ -7,9 +6,8 @@
 #SBATCH --ntasks=37
 #SBATCH -p workq
 
-module swap gcc/5.5.0 gcc/4.8.5
-module load python/3.6.3 mpi4py
+module load python
+module load mpi4py
 
 set -ex
-cd /group/courses0100/phancock/mpi4py
 srun -n 37 python hello.py
