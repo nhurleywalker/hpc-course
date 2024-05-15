b649abb8d79caa7c42052686c676044c8b163b3c
Natasha Hurley-Walker <nhw@icrar.org>
2024-05-15 15:58:19 +0800

    Updated with latest module loads for setonix

--- hello.sh
+++ hello.sh
@@ -6,8 +6,8 @@
 #SBATCH --ntasks=12
 #SBATCH -p work
 
-module load python/3.9.15
-module load py-mpi4py/3.1.2-py3.9.15
+module load python/3.10.10
+module load py-mpi4py/3.1.4-py3.10.10
 
 set -ex
 srun -n 12 python hello.py
